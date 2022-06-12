0faa8a10169389a1d8850e5f7e421b0019600403
Agustin Leguizamon <agustin.leg7@gmail.com>
2022-06-12 00:21:31 -0300

    test de carga

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,10 @@ const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
 const dotenv = require('dotenv')
+const crypto = require('crypto')
+
+const randomRoute = require('./routes/randomRoute')
+const infoRoute = require('./routes/info')
 
 const PORT = process.argv[2] || 3000
 const modoCluster = process.argv[3] === 'CLUSTER'
@@ -36,6 +40,65 @@ if (modoCluster && cluster.isMaster) {
       if (isPrime(i)) primes.push(i)
     }
     res.json(primes)
+  })
+
+  const users = {}
+
+  app.get('/getUsers', (req, res) => {
+    res.json({ users })
+  })
+
+  app.get('/newUser', (req, res) => {
+    let userName = req.query.userName || ''
+    let password = req.query.password || ''
+
+    if (!userName || !password || users[userName]) return res.sendStatus(400)
+
+    const salt = crypto.randomBytes(128).toString('base64')
+    const hash = crypto.pbkdf2Sync(password, salt, 10000, 512, 'sha512')
+
+    users[userName] = { salt, hash }
+
+    res.status(200).json(users)
+  })
+
+  app.get('/auth-bloq', (req, res) => {
+    let userName = req.query.userName || ''
+    let password = req.query.password || ''
+
+    if (!userName || !password || !users[userName]) process.exit(-1)
+
+    const { salt, hash } = users[userName]
+
+    const encryptHash = crypto.pbkdf2Sync(password, salt, 10000, 512, 'sha512')
+
+    if (crypto.timingSafeEqual(hash, encryptHash)) {
+      res.sendStatus(200)
+    } else {
+      process.exit(-1)
+    }
+  })
+
+  app.get('/auth-nobloq', (req, res) => {
+    let userName = req.query.userName || ''
+    let password = req.query.password || ''
+
+    if (!userName || !password || !users[userName]) process.exit(-1)
+
+    crypto.pbkdf2(
+      password,
+      users[userName].salt,
+      10000,
+      512,
+      'sha512',
+      (err, hash) => {
+        if (users[userName].hash.toString() === hash.toString()) {
+          res.sendStatus(200)
+        } else {
+          process.exit(-1)
+        }
+      }
+    )
   })
 
   const server = app.listen(PORT, () =>
